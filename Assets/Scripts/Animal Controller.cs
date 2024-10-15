5a1ce251a116aada53b003ea8f87d924c1804472
Ashraful Kadir <ashraful@gmail.com>
2024-10-15 18:33:08 +0200

    Animals can be pulled from prefab.

--- Assets/Scripts/Animal Controller.cs
+++ Assets/Scripts/Animal Controller.cs
@@ -5,25 +5,56 @@ using UnityEngine;
 public class AnimalController : MonoBehaviour
 {
     public float speed;
+    private float randomSeconds;
+    private float minSeconds = 10.0f;
+    private float maxSeconds = 60.0f;
+    private float secondsMeasure = 0.0f;
+    private float minSpeed, maxSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomSeconds = Random.Range(minSeconds,maxSeconds);
+        minSpeed = speed / 2.0f;
+        maxSpeed = speed * 2.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * Time.deltaTime * speed;
+
+        secondsMeasure += Time.deltaTime;
+
+        if (secondsMeasure > randomSeconds)
+        {
+            float angle = Random.Range(0.0f, 360.0f);
+            transform.Rotate(Vector3.up, angle);
+
+            speed = Random.Range(minSpeed,maxSpeed);
+
+            randomSeconds = Random.Range(minSeconds, maxSeconds);
+            secondsMeasure = 0.0f;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
 
-        if (other.gameObject.CompareTag("Boundary Object") || other.gameObject.CompareTag("Animal"))
+        if (other.gameObject.CompareTag("Boundary Object"))
         {
             transform.forward = -transform.forward;
         }
+        else if (other.gameObject.CompareTag("Animal")) // Anmials will take u-turns when they collide
+        {
+            transform.forward = -transform.forward;
+            other.transform.forward = -other.transform.forward;
+        }
+        else if (other.gameObject.CompareTag("Human")) // Animals will take u-turn when collide with humans
+        {
+            transform.forward = -transform.forward;
+        }
+
     }
 }
--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -6,16 +6,76 @@ public class PlayerController : MonoBehaviour
 {
     public Camera topCamera;
     public Camera mainCamera;
+    public Camera thirdCamera;
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    private float xRange = 30.0f;
+    private float zRange = 30.0f;
+    private float xStart;
+    private float zStart;
+    private float xMin, zMin, xMax, zMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        topCamera.enabled = false;
+        mainCamera.enabled = false;
+        thirdCamera.enabled = true;
+
+        xStart = transform.position.x;
+        xMin = xStart - xRange;
+        xMax = xStart + xRange;
+        zStart = transform.position.z;
+        zMin = zStart - zRange;
+        zMax = zStart + zRange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Movinng the player forward or backward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Turning the player
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
+        // Switching camera view
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            topCamera.enabled = true;
+            mainCamera.enabled = false;
+            thirdCamera.enabled = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            topCamera.enabled = false;
+            mainCamera.enabled = true;
+            thirdCamera.enabled = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            topCamera.enabled = false;
+            mainCamera.enabled = false;
+            thirdCamera.enabled = true;
+        }
+
+
+        // Limiting the player withing a rectangluar domain
+        if (transform.position.x > xMax)
+            transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
+        if (transform.position.x < xMin)
+            transform.position = new Vector3(xMin, transform.position.y, transform.position.z);
+
+        if (transform.position.z > zMax)
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        if (transform.position.z < zMin)
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
     }
 }
